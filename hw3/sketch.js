eacd12d5de67af69d6df3dc2ff74859448cddd1a
julianrileyy <julianriley312@vt.edu>
2026-02-07 19:53:08 -0500

    textures

--- hw3/sketch.js
+++ hw3/sketch.js
@@ -1,3 +1,9 @@
+let img;
+function preload() {
+ img = loadImage('caketexture.jpg');
+img2 = loadImage('insidetexture.png');
+}
+    
 function setup() {
     createCanvas(400,400, WEBGL);
 }
@@ -7,12 +13,13 @@ function draw() {
     orbitControl();
     ambientLight(128, 128, 128);
     directionalLight(128, 128, 128, 0, 0, -1);
-    
+    noStroke();
 //Bottom Layer
 push();
 translate(0,50,0); //positioning
 fill(245,222,179);
 noStroke();
+ texture(img);
 cylinder(100,60); //radius,height
 pop();
     
@@ -21,90 +28,72 @@ push();
 translate(0,0,0);
 fill(255, 221, 202);
 noStroke();
+texture(img2);
 cylinder(70,60);
 pop();
     
 //Candle1
 push();
 translate(40, -40, 0); // Position on cake
-fill(227, 228, 250);      
+fill(131, 92, 59);      
 cylinder(3, 30);      
 pop();
 //Flame1
 push();
 translate(40, -60, 0); 
-fill(238, 154, 77);    
+fill(255, 232, 124);    
 sphere(4);            
 pop();
     
 //Candle2
 push();
 translate(-40, -40, 0); // Position on cake
-fill(227, 228, 250);      
+fill(131, 92, 59);      
 cylinder(3, 30);      
 pop();
 //Flame2
 push();
 translate(-40, -60, 0); 
-fill(238, 154, 77);    
+fill(255, 232, 124);    
 sphere(4);            
 pop();
     
 //Candle3
 push();
 translate(0, -40, 40); // Position on cake
-fill(227, 228, 250);      
+fill(131, 92, 59);      
 cylinder(3, 30);      
 pop();
 //Flame3
 push();
 translate(0, -60, 40); 
-fill(238, 154, 77);    
+fill(255, 232, 124);    
 sphere(4);            
 pop();
     
 //Candle4
 push();
 translate(0, -40, -40); // Position on cake
-fill(227, 228, 250);      
+fill(131, 92, 59);      
 cylinder(3, 30);      
 pop();
 //Flame4
 push();
 translate(0, -60, -40); 
-fill(238, 154, 77);    
+fill(255, 232, 124);    
 sphere(4);            
 pop();
     
 //Candle5
 push();
 translate(0, -40, 0); // Position on cake
-fill(227, 228, 250);      
+fill(131, 92, 59);      
 cylinder(3, 30);      
 pop();
 //Flame5
 push();
 translate(0, -60, 0); 
-fill(238, 154, 77);    
-sphere(4);            
-pop();
-    
-//Sprinkles
-push();
-translate(0, 40, 100); 
-fill(238, 154, 77);    
-sphere(4);            
-pop();
-    
-push();
-translate(30, 60, 95); 
-fill(238, 154, 77);    
-sphere(4);            
-pop();
-
-push();
-translate(45, 30, 90); 
-fill(238, 154, 77);    
+fill(255, 232, 124);    
 sphere(4);            
 pop();
 }
